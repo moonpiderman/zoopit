323e340add870ff7793e553d933e752637da11b3
moonpiderman <lotsofchuck@gmail.com>
2022-10-04 11:01:35 +0900

    Comment: Test code application

--- src/test/kotlin/zoopit/emergency/zoopitserver/ZoopitServerApplicationTests.kt
+++ src/test/kotlin/zoopit/emergency/zoopitserver/ZoopitServerApplicationTests.kt
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest
 @SpringBootTest
 class ZoopitServerApplicationTests {
 
-	@Test
-	fun contextLoads() {
-	}
+//	@Test
+//	fun contextLoads() {
+//	}
 
 }
